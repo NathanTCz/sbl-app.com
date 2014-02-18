70d1abefabe2e33e8575e689b8ee45cbed73dcab
NathanTCz <nathancazell@gmail.com>
2014-02-18 17:06:03 -0500

    modfied git-pull.php

--- git-pull.php
+++ git-pull.php
@@ -3,6 +3,9 @@ echo `ls | grep -v git-pull | xargs rm -rf`;
 echo `git clone https://github.com/NathanTCz/sbl-app.com.git`;
 echo `mv sbl-app.com/* sbl-app.com/.* ./`;
 echo `rm -rf sbl-app.com`;
-echo `whoami`;
+echo `cp ../config/sbl_config.php core/config.php`;
+echo `mkdir logs`;
+echo `touch logs/access.log`;
+echo `touch logs/error.log`;
+echo `touch logs/rewrite.log`;
 ?>
-
