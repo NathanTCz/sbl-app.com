f6bd1c27b98cc65879a921aa8a3bdec997a7c352
Nick Castelli <nvc11@my.fsu.edu>
2014-02-28 11:52:59 -0500

    changed some of the greeting messages and other text stuff

--- includes/pages/404.php
+++ includes/pages/404.php
@@ -1,5 +1,5 @@
 <div class="widget" id="home">
 	<p>
-	<b>Well this is embarassing... We couldn't find the page you're looking for.</b>
+	<b>Well this is embarassing... We couldn't find the page you're looking for.(Probaly because you typed something in wrong.)</b>
 	</p>
 </div>
