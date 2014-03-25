b7c6ac791b6430756fada23f37f7db307286c6ae
NathanTcz <nathancazell@gmail.com>
2014-03-25 14:22:59 -0400

    forgot to add data_init.php

---
+++ core/data_init.php
@@ -0,0 +1,16 @@
+  <?php
+  /* sort function helper for sorting events by time
+   * under date
+   */
+  function compare_event_time($a, $b) {
+    $a_time = strtotime($a->timestamp);
+    $b_time = strtotime($b->timestamp);
+
+    if ($a_time == $b_time)
+      return 0;
+
+    return ($a_time < $b_time) ? -1 : 1;
+  }
+
+  //usort($EVENTS, 'compare_event_time');
+?>
