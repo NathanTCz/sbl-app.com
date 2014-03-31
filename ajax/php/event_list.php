4e97639eb5d38bd6ef3463c337c99f6bbe662c2f
Benjamin Patton <sheebyboy@gmail.com>
2014-03-30 22:27:16 -0400

    Added functions to set & get recently won/lost bets, added functionality so user gets 50 credits after 3 weeks, additional error checking added

--- ajax/php/event_list.php
+++ ajax/php/event_list.php
@@ -1,14 +1,27 @@
 <?php
-/* Move back to the server root dir so that
- * so that includes and such work correctly
- */
+
+/*
+   Author: Benjamin Patton (aslo including project partners Nick Castelli, 
+           Joe Deleeuw, and Nathan Cazell)
+   Module Name: event_list.php                               
+   Description: This module is used on our website to display events to the user. It sorts 
+   the events and uses a front end css to display the events in a user friendly 
+   fashion. Also included in in-line comments as part of description
+   Date Created: 2/27/2014 
+*/
+
+/* 
+  This moves back to the server root dir so that
+  so that includes and such work correctly
+*/
 $root_dir = $_SERVER['DOCUMENT_ROOT'];
 chdir($root_dir);
 
+//Have to require the init.php file to interact with the database correctly
 require_once 'core/init.php';
 
-
-
+//Function takes in 2 dates to compare them in order to display events 
+//in the correct order by date
 function compare_event_date($a, $b) {
   $a_date = strtotime($a);
   $b_date = strtotime($b);
@@ -19,10 +32,13 @@ function compare_event_date($a, $b) {
   return ($a_date < $b_date) ? -1 : 1;
 }
 
+//Gets the category id of an event
 $cat_id = $_GET['cat'];
+
+//Array holding all of the dates of the events
 $dates = array();
 
-// Store dates for categorization
+//Stores the dates for categorization based on the category id
 foreach ($EVENTS as $event) {
   if ( !in_array($event->date, $dates)
       &&
@@ -34,8 +50,11 @@ foreach ($EVENTS as $event) {
     $dates[] = $event->date;
 }
 
+//Built in php function to sort the dates as defined in the 
+//in the function above
 usort($dates, 'compare_event_date');
 
+//Goes through each date in the array and displays the dates
 foreach ($dates as $date) {
 ?>
 <div class="date_title">
@@ -48,6 +67,9 @@ foreach ($dates as $date) {
 </div>
 
 <?php
+  //Goes through each event in the database and gets the category, 
+  //and will then display the event under the correct date determined 
+  //previously by going through the events array
   foreach ($EVENTS as $event) {
     $cat_name = $event->category->name;
 
@@ -71,7 +93,7 @@ foreach ($dates as $date) {
           ?>
         </span>
         <span>
-          <?php echo
+          <?php echo //Display who is facing each other in the event
             $event->away_team->name
             . ' vs ' .
             $event->home_team->name
@@ -79,13 +101,13 @@ foreach ($dates as $date) {
           ?>
         </span>
         <span>
-          <?php echo
+          <?php echo //Diplay the beginning time of the event
             $event->time
             ;
           ?>
         </span>
         <span>
-          <?php echo
+          <?php echo //Display the scores of the two teams 
             $event->away_score
             . ' - ' .
             $event->home_score
@@ -93,7 +115,7 @@ foreach ($dates as $date) {
           ?>
         </span>
         <span>
-          <?php echo
+          <?php echo //Diplay the discription of the event
             $event->description
             ;
           ?>
